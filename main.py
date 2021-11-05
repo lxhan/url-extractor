d226969fb58a3ef0b967cce107b5f636418cd304
Alex Han <alex@craveup.com>
2021-11-05 14:19:21 +0900

    add loop & formating

--- main.py
+++ main.py
@@ -9,23 +9,32 @@ chrome_options.add_argument("--headless")
 
 driver = webdriver.Chrome(options=chrome_options)
 
-input_url = input("Input video url:\n")
+on = True
+while on:
+    input_url = input("Input video url:\n")
 
-driver.get(input_url)
+    if input_url == "done":
+        on  = False
+        break
 
-video_url = ""
-pk = ""
-for request in driver.requests:
-    if request.response and API_VIDEO_URL in request.url:
-        video_url = request.url
-        pk = request.headers.get("accept")
+    driver.get(input_url)
 
-if video_url:
-    headers = {
-        "accept": pk
-    }
-    r = requests.get(url=video_url, headers=headers)
-    data = r.json()
-    sources = data["sources"]
-    src_url = sources[-1]["src"]
-    print(src_url)
+    video_url = ""
+    pk = ""
+    for request in driver.requests:
+        if request.response and API_VIDEO_URL in request.url:
+            video_url = request.url
+            pk = request.headers.get("accept")
+
+    if video_url:
+        headers = {
+            "accept": pk
+        }
+        r = requests.get(url=video_url, headers=headers)
+        data = r.json()
+        sources = data["sources"]
+        src_url = sources[-1]["src"]
+
+        print("\n---------------------------------------------------------------------------------")
+        print(src_url)
+        print("---------------------------------------------------------------------------------\n")
